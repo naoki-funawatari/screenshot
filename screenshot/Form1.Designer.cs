8a3ecb3be1bce9e989c8d344adaa556170b48692
naoki-funawatari <40711643+naoki-funawatari@users.noreply.github.com>
2019-08-20 11:47:52 +0900

    version1.0

---
+++ screenshot/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace screenshot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.main_screen = new System.Windows.Forms.Button();
+            this.sub_screen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // main_screen
+            // 
+            this.main_screen.Location = new System.Drawing.Point(12, 12);
+            this.main_screen.Name = "main_screen";
+            this.main_screen.Size = new System.Drawing.Size(100, 100);
+            this.main_screen.TabIndex = 0;
+            this.main_screen.Text = "Main";
+            this.main_screen.UseVisualStyleBackColor = true;
+            this.main_screen.Click += new System.EventHandler(this.main_screen_Click);
+            // 
+            // sub_screen
+            // 
+            this.sub_screen.Location = new System.Drawing.Point(124, 12);
+            this.sub_screen.Name = "sub_screen";
+            this.sub_screen.Size = new System.Drawing.Size(100, 100);
+            this.sub_screen.TabIndex = 1;
+            this.sub_screen.Text = "Sub";
+            this.sub_screen.UseVisualStyleBackColor = true;
+            this.sub_screen.Click += new System.EventHandler(this.sub_screen_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(236, 123);
+            this.Controls.Add(this.sub_screen);
+            this.Controls.Add(this.main_screen);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "screenshot";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button main_screen;
+        private System.Windows.Forms.Button sub_screen;
+    }
+}
+
---
+++ screenshot/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace screenshot
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void main_screen_Click(object sender, EventArgs e)
+        {
+
+            Screen s = Screen.AllScreens[0];
+            int x = s.Bounds.X;
+            int y = s.Bounds.Y;
+            int w = s.Bounds.Width;
+            int h = s.Bounds.Height;
+
+            using (Bitmap b = new Bitmap(w, h))
+            using (Graphics g = Graphics.FromImage(b))
+            {
+                g.CopyFromScreen(new Point(x, y), new Point(0, 0), b.Size);
+                Clipboard.SetImage(b);
+            }
+        }
+
+        private void sub_screen_Click(object sender, EventArgs e)
+        {
+
+            Screen s = Screen.AllScreens[1];
+            int x = s.Bounds.X;
+            int y = s.Bounds.Y;
+            int w = s.Bounds.Width;
+            int h = s.Bounds.Height;
+
+            using (Bitmap b = new Bitmap(w, h))
+            using (Graphics g = Graphics.FromImage(b))
+            {
+                g.CopyFromScreen(new Point(x, y), new Point(0, 0), b.Size);
+                Clipboard.SetImage(b);
+            }
+        }
+    }
+}
